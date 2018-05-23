67487da7ab636077943c737a7bc5aeeb4a65934a
AngelicaMColom <angelidu@gmail.com>
2018-05-23 00:36:34 -0500

    added depentencies

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,9 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
           <h1 className="App-title">Mi primera aplicación</h1>
-        </header>
         <p className="App-intro">
           Esta es mi primera aplicación en React, está padrísimo!
         </p>
